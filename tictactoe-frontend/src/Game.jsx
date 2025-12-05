8e87861fbea1368c133495864493845c72843d87
usr1l <zhengtony46@gmail.com>
2025-12-05 04:59:57 -0500

    walletconnect

---
+++ tictactoe-frontend/src/Game.jsx
@@ -0,0 +1,10 @@
+import {
+    useReadContracts,
+    useWriteContract,
+    useWaitForTransactionReceipt,
+    useAccount
+} from 'wagmi';
+
+import { TICTACTOE_ADDRESS, TICTACTOE_ABI } from './config/config';
+
+export default function Game() {}
